fa45867b606a1415884fccef8ce9eb92ffb44f0c
Hossam Ghareeb <hossam.ghareb@gmail.com>
2016-11-04 11:15:48 +0400

    Adding MVVM folders

---
+++ TweetsSearch/TweetsSearch/View/TweetSearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TweetSearchViewController.swift
+//  TweetsSearch
+//
+//  Created by Hossam Ghareeb on 11/4/16.
+//  Copyright © 2016 Hossam Ghareeb. All rights reserved.
+//
+
+import UIKit
+
+class TweetSearchViewController: UIViewController {
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var tweetSearchBar: UISearchBar!
+    @IBOutlet weak var tweetsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ TweetsSearch/TweetsSearch/ViewModel/TweetSearchViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TweetSearchViewModel.swift
+//  TweetsSearch
+//
+//  Created by Hossam Ghareeb on 11/4/16.
+//  Copyright © 2016 Hossam Ghareeb. All rights reserved.
+//
+
+import UIKit
+
+class TweetSearchViewModel: NSObject {
+
+}
