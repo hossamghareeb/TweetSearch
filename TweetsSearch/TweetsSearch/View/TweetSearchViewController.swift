7f8e077ccdca7867f3bab1b45dd64f9f9a53a769
Hossam Ghareeb <hossam.ghareb@gmail.com>
2016-11-06 11:50:44 +0400

    Adding Bond Library for binding

--- TweetsSearch/TweetsSearch/View/TweetSearchViewController.swift
+++ TweetsSearch/TweetsSearch/View/TweetSearchViewController.swift
@@ -7,16 +7,31 @@
 //
 
 import UIKit
+import Bond
+import ReactiveKit
 
 class TweetSearchViewController: UIViewController {
 
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var tweetSearchBar: UISearchBar!
     @IBOutlet weak var tweetsTableView: UITableView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    let viewModel = TweetSearchViewModel()
+    
+    var text: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        bindViewModel()
+    }
+    
+    func bindViewModel(){
+        self.viewModel.searchString.bidirectionalBind(to: self.searchTextField.bnd_text)
+        self.viewModel.validSearchString
+            .map{$0 ? UIColor.black : UIColor.red}
+            .bind(to: self.searchTextField.bnd_textColor)
+        self.viewModel.isSearching.bind(to: self.activityIndicator.bnd_animating)
     }
 }
--- TweetsSearch/TweetsSearch/ViewModel/TweetSearchViewModel.swift
+++ TweetsSearch/TweetsSearch/ViewModel/TweetSearchViewModel.swift
@@ -7,7 +7,35 @@
 //
 
 import UIKit
+import Bond
+import ReactiveKit
 
-class TweetSearchViewModel: NSObject {
+class TweetSearchViewModel {
+    
+    private struct Constants{
+        static let MinimumSearchLength = 3
+        static let SearchThrottleTimeInSeconds: TimeInterval = 0.5
+    }
 
+    let searchString = Observable<String?>("")
+    let validSearchString = Observable<Bool>(false)
+    let isSearching = Observable<Bool>(false)
+    
+    init() {
+        searchString.value = "" // default value in text field of search
+
+        searchString
+            .map { $0!.characters.count > Constants.MinimumSearchLength }
+            .bind(to: validSearchString)
+        _ = searchString
+            .filter{$0!.characters.count > Constants.MinimumSearchLength }
+            .throttle(seconds: Constants.SearchThrottleTimeInSeconds)
+            .observeNext{ [unowned self] text in
+                self.startTweetSearching(searchText: text!)
+        }
+    }
+    
+    func startTweetSearching(searchText text: String){
+        print(text)
+    }
 }
